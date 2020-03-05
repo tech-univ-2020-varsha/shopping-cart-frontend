51e6f5a999319e0b0172041ce3e7b5ada938117b
Varsha C L <clvarsha1998@gmail.com>
2020-03-06 02:41:22 +0530

    feat:add checkout and continue shopping button functionality

--- src/Components/SummaryCard/summaryCard.jsx
+++ src/Components/SummaryCard/summaryCard.jsx
@@ -1,35 +1,60 @@
 import React from 'react';
 import propTypes from 'prop-types';
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
 import * as styles from './summaryCard.module.css';
+import URL from '../../constants/url';
 
-const SummaryCard = ({ cartTotal }) => (
-  <div className={styles.container}>
-    <div className={styles.summaryDetails}>
-      <div className={styles.priceDetails}>
-        <div className={styles.totalWord}>
+const checkout = async (history) => {
+  const response = await axios.get(`${URL}/checkout`);
+  if (response.status === 200) {
+    history.push('/');
+  }
+};
 
-          Total
+const continueShopping = (history) => {
+  history.push('/');
+};
+
+const SummaryCard = ({ cartTotal }) => {
+  const history = useHistory();
+  return (
+    <div className={styles.container}>
+      <div className={styles.summaryDetails}>
+        <div className={styles.priceDetails}>
+          <div className={styles.totalWord}>
+
+            Total
+          </div>
+          <div className={styles.priceCartTotal}>
+            <span className={styles.RS}>RS</span>
+            {' '}
+            {cartTotal}
+          </div>
         </div>
-        <div className={styles.priceCartTotal}>
-          <span className={styles.RS}>RS</span>
-          {' '}
-          {cartTotal}
+        <hr className={styles.line} />
+        <div className={styles.checkout}>
+          <button
+            type="submit"
+            className={styles.checkoutBtn}
+            onClick={() => { checkout(history); }}
+          >
+            Checkout
+          </button>
         </div>
       </div>
-      <hr className={styles.line} />
       <div className={styles.checkout}>
-        <button type="submit" className={styles.checkoutBtn}>
-          Checkout
+        <button
+          type="submit"
+          className={styles.continueBtn}
+          onClick={() => { continueShopping(history); }}
+        >
+          Continue Shopping
         </button>
       </div>
     </div>
-    <div>
-      <button type="submit">
-        Continue Shopping
-      </button>
-    </div>
-  </div>
-);
+  );
+};
 
 SummaryCard.propTypes = {
   cartTotal: propTypes.number.isRequired,
